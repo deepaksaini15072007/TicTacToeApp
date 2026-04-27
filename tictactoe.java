178c6df21655b663fa7572adbd407922dc4eef57
Deepak saini <deepaksaini15072007@gmail.com>
2026-04-27 23:10:32 +0530

    feat:add uc3

--- tictactoe.java
+++ tictactoe.java
@@ -1,46 +1,22 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class tictactoe {
 
-    static boolean isHumanTurn;
-    static char humanSymbol;
-    static char computerSymbol;
-
     public static void main(String[] args) {
 
-        tossAndAssignSymbols();
-        displayTossResult();
+        int slot = getUserSlot();
+
+        System.out.println("Slot entered: " + slot);
     }
 
-    static void tossAndAssignSymbols() {
+    static int getUserSlot() {
 
-        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
 
-        int toss = random.nextInt(2);
+        System.out.print("Enter slot number (1-9): ");
 
-        if (toss == 0) {
+        int slot = sc.nextInt();
 
-            isHumanTurn = true;
-            humanSymbol = 'X';
-            computerSymbol = 'O';
-
-        } else {
-
-            isHumanTurn = false;
-            humanSymbol = 'O';
-            computerSymbol = 'X';
-        }
-    }
-
-    static void displayTossResult() {
-
-        if (isHumanTurn) {
-            System.out.println("Human starts first.");
-        } else {
-            System.out.println("Computer starts first.");
-        }
-
-        System.out.println("Human Symbol: " + humanSymbol);
-        System.out.println("Computer Symbol: " + computerSymbol);
+        return slot;
     }
 }
