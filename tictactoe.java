67b2182808f844625c7d80027f1010b609879d7c
Deepak saini <deepaksaini15072007@gmail.com>
2026-05-02 00:13:29 +0530

    feat:add uc5

--- tictactoe.java
+++ tictactoe.java
@@ -1,20 +1,25 @@
-public class TicTacToe {
+public class tictactoe {
+
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
 
     public static void main(String[] args) {
 
-        int slot = 7;
-
-        System.out.println("Row: " + getRowFromSlot(slot));
-        System.out.println("Column: " + getColFromSlot(slot));
+        System.out.println(isValidMove(1, 1));
     }
 
-    static int getRowFromSlot(int slot) {
+    static boolean isValidMove(int row, int col) {
 
-        return (slot - 1) / 3;
-    }
+        if (row >= 0 && row < 3 && col >= 0 && col < 3) {
 
-    static int getColFromSlot(int slot) {
+            if (board[row][col] == '-') {
+                return true;
+            }
+        }
 
-        return (slot - 1) % 3;
+        return false;
     }
 }
