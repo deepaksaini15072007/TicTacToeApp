165163006523b79f0bca325a08ae6f4e547556bb
Deepak saini <deepaksaini15072007@gmail.com>
2026-05-02 00:52:43 +0530

    feat : add uc10

--- tictactoe.java
+++ tictactoe.java
@@ -1,49 +1,28 @@
 public class tictactoe {
 
     static char[][] board = {
-            {'X', 'X', 'X'},
-            {'-', '-', '-'},
-            {'-', '-', '-'}
+            {'X', 'O', 'X'},
+            {'X', 'O', 'O'},
+            {'O', 'X', 'X'}
     };
 
     public static void main(String[] args) {
 
-        System.out.println(hasWon('X'));
+        System.out.println(isDraw());
     }
 
-    static boolean hasWon(char symbol) {
+    static boolean isDraw() {
 
-    
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol &&
-                board[i][1] == symbol &&
-                board[i][2] == symbol) {
-                return true;
+        for (int r = 0; r < 3; r++) {
+
+            for (int c = 0; c < 3; c++) {
+
+                if (board[r][c] == '-') {
+                    return false;
+                }
             }
         }
 
-    
-        for (int j = 0; j < 3; j++) {
-            if (board[0][j] == symbol &&
-                board[1][j] == symbol &&
-                board[2][j] == symbol) {
-                return true;
-            }
-        }
-
-        
-        if (board[0][0] == symbol &&
-            board[1][1] == symbol &&
-            board[2][2] == symbol) {
-            return true;
-        }
-
-        if (board[0][2] == symbol &&
-            board[1][1] == symbol &&
-            board[2][0] == symbol) {
-            return true;
-        }
-
-        return false;
+        return true;
     }
 }
