5068469192eab4ac8d9fca65f172d482bffe19bd
Deepak saini <deepaksaini15072007@gmail.com>
2026-05-02 00:29:38 +0530

    feat:add uc7

--- tictactoe.java
+++ tictactoe.java
@@ -1,16 +1,51 @@
+import java.util.Random;
+
 public class tictactoe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
 
-        placeMove(0, 0, 'X');
+        computerMove();
 
-        System.out.println(board[0][0]);
+        printBoard();
     }
 
-    static void placeMove(int row, int col, char symbol) {
+    static void computerMove() {
 
-        board[row][col] = symbol;
+        Random random = new Random();
+
+        while (true) {
+
+            int slot = random.nextInt(9) + 1;
+
+            int row = (slot - 1) / 3;
+            int col = (slot - 1) % 3;
+
+            if (board[row][col] == '-') {
+
+                board[row][col] = computerSymbol;
+                break;
+            }
+        }
+    }
+
+    static void printBoard() {
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+
+                System.out.print(board[i][j] + " ");
+            }
+
+            System.out.println();
+        }
     }
 }
